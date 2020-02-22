2e7f1cc921da797e1b162cb06680231626ab24a6
Soldeplata Saketos Candela <soldeplatadeveloper@gmail.com>
2020-02-22 10:19:50 +0100

    initial commit

---
+++ src/serve-dev.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import Vue from 'vue';
+import VuePopper from '@/vue-popper.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VuePopper
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-popper />
+  </div>
+</template>
